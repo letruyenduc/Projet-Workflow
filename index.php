c3f3554775468e0999cb197883f1167736dbc1d3
David <david.lpnc@gmail.com>
2025-11-17 15:56:13 +0100

    Create index.php with basic HTML structure
    Button

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <h1>Workflow</h1>
+</header>
+
+
+
+   <middle>
+    <button onclick="location.href='contact.php'">Contact</button>
+    <button onclick="location.href='shop.php'">Produits</button>
+</middle>
+    
+</body>
+</html>
